89848d4b569ddc91bb1a1f3bcbec50cd06bcab87
SOUSA,J <35965251+JrSousaJ@users.noreply.github.com>
2019-09-09 19:32:33 -0300

    feat: main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#include "livros.h"
+
+
+int main()
+{
+	adicionarLivros(livro);
+	adicionarLivros(livro);
+	excluirLivro(livro,0);
+	visualizarLivro(livro,0);
+	return 0;
+}
